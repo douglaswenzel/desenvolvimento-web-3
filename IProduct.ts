694f62b7d4ac2a00ad365e6a714cfd633eec1d3b
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-04-01 20:13:48 -0300

    IProduct interface criada

---
+++ IProduct.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    name: string;
+    brand: string;
+    barCode: string;
+    supplier: string;
+    stockId: number;
+    price: number;
+    weight: number;
+    measureUnit: string;
+  }
